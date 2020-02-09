73a0bc085d0e579b055e87b07409264c469e917c
gengjihui_maya@163.com <gengjihui_maya@163.com>
2020-02-09 09:35:12 +0800

    mybatis:          增删改时，需要使用commit()方法，或者，设置openSession(true),自动提交,一般，手动提交，          另外，SQL执行出错时，要使用rollBack()方法，进行事务回滚

--- src/com/geng/test/Test.java
+++ src/com/geng/test/Test.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * mybatis+log4j
@@ -22,10 +25,35 @@ public class Test {
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
         //工厂生产对应的session会话
         SqlSession session = factory.openSession();
-        session.selectList("com.geng.mapper.PeopleMapper.selectPeople");
-        //测试传参查询单行people数据:selectOne(指定标签id名：namespace+id,id);
-        People p = session.selectOne("com.geng.mapper.PeopleMapper.selectOne", 1);
-        System.out.println("测试传参查询单行people数据:"+p);
+//        session.selectList("com.geng.mapper.PeopleMapper.selectPeople");
+//        //测试传参查询单行people数据:selectOne(指定标签id名：namespace+id,id);
+//        People p = session.selectOne("com.geng.mapper.PeopleMapper.selectOne", 1);
+//        System.out.println("测试传参查询单行people数据:" + p);
+//        Map<String, Object> map = new HashMap<>();
+//        //显示数据数
+//        int pageSize = 2;
+//        //要求从哪里开始
+//        int pageNumber = 1;
+//        //显示页数
+//        map.put("pageSize", pageSize);
+//        //经计算后的当前页数
+//        map.put("pageStart", pageSize * (pageNumber - 1));
+//        List<People> list = session.selectList("com.geng.mapper.PeopleMapper.page", map);
+//        System.out.println(list);
+        //新增people信息
+        People people = new People();
+        people.setName("新增name");
+        people.setAge(33);
+        int insertResault = session.insert("com.geng.mapper.PeopleMapper.insertPeopleInformation", people);
+        //mybatis底层使用的是JDBC，而JDBC默认是不自动提交事务的，
+        // 所以，在执行增删改时，我们要么在执行完SQL语句之后手动提交事务（调用commit()），
+        // 要么使factory.openSession(true)方法，设置为自动提交事务
+        session.commit();
+        if (insertResault > 0) {
+            System.out.println("新增成功");
+        } else {
+            System.out.println("新增失败");
+        }
         session.close();
     }
 }
